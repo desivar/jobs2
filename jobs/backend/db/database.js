db2788dae96736d4dcb1c5b0336da47bd143e5e8
desivar <jillian_desire@yahoo.es>
2025-06-14 15:48:51 -0600

    database

--- jobs/backend/db/database.js
+++ jobs/backend/db/database.js
@@ -1,24 +1,14 @@
-// db/database.js
-
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-
-let _db;
+// backend/db/database.js
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  if (_db) {
-    console.log('MongoDB already connected.');
-    return _db;
-  }
-
-  const uri = process.env.MONGODB_URI;
-  if (!uri) throw new Error('MONGODB_URI is not defined.');
-
-  const client = new MongoClient(uri);
-  await client.connect();
-  _db = client;
-  console.log('MongoDB connected.');
-  return _db;
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
+    }
 };
 
 module.exports = connectDB;
