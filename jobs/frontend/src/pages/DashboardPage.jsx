1853163c5eab812237b84c7ba0a32f0349c8f940
desivar <jillian_desire@yahoo.es>
2025-06-15 17:13:34 -0600

    DashboardPage.jsx

--- jobs/frontend/src/pages/DashboardPage.jsx
+++ jobs/frontend/src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../api/UserProfilePage';
+import { useAuth } from '../api/users';
 import { usePipelines } from '../api/pipelines';
 import { 
   BarChart, 
