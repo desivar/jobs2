8d11b1d79234cc8068c96b0bbb726147964c74d5
desivar <jillian_desire@yahoo.es>
2025-06-15 17:32:25 -0600

    fix errors in dashboard

--- jobs/frontend/src/pages/DashboardPage.jsx
+++ jobs/frontend/src/pages/DashboardPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../api/users';
+import { useAuth } from '../api/UserProfilePage';
 import { usePipelines } from '../api/pipelines';
 import { 
   BarChart, 
