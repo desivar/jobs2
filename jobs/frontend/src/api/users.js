0d0e97eed15991912f760e6fb7338db425c10d09
desivar <jillian_desire@yahoo.es>
2025-06-15 16:51:07 -0600

    users.js

---
+++ jobs/frontend/src/api/users.js
@@ -0,0 +1,27 @@
+// src/api/users.js
+import apiClient from './apiClient';
+
+export const getAllUsers = async () => {
+  const response = await apiClient.get('/users');
+  return response.data;
+};
+
+export const getUserById = async (id) => {
+  const response = await apiClient.get(`/users/${id}`);
+  return response.data;
+};
+
+export const createUser = async (userData) => {
+  const response = await apiClient.post('/users', userData);
+  return response.data;
+};
+
+export const updateUser = async (id, userData) => {
+  const response = await apiClient.put(`/users/${id}`, userData);
+  return response.data;
+};
+
+export const deleteUser = async (id) => {
+  const response = await apiClient.delete(`/users/${id}`);
+  return response.data;
+};
